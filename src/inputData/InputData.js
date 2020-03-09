45b36d12f0aab7d8b4e9ad0cd4fad59ee5acc05d
NicoCardenas <nicolas.cardenas@mail.escuelaing.edu.co>
2020-03-08 22:18:34 -0500

    first commit

---
+++ src/inputData/InputData.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import Axios from 'axios';
+import PropTypes from 'prop-types';
+import { Paper, TextField, Button } from '@material-ui/core';
+
+export class InputData extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: '', lastName: '', age: '' }
+  }
+
+  paper = {
+    marginTop: "2vh",
+    padding: "2%"
+  }
+
+  input = {
+    marginTop: 8,
+    marginLeft: 5,
+    marginRight: 5,
+    marginBottom: 8
+  }
+
+  handleName = (event) => {
+    this.setState({ name: event.target.value });
+  }
+
+  handleLastName = (event) => {
+    this.setState({ lastName: event.target.value });
+  }
+
+  handleAge = (event) => {
+    this.setState({ age: event.target.value });
+  }
+
+  handleClick = (event) => {
+    event.preventDefault();
+
+    if (this.state.name.length === 0)
+      return;
+    if (this.state.lastName.length === 0)
+      return;
+    if (this.state.age.length === 0)
+      return;
+
+    const user = {
+      name: this.state.name,
+      lastName: this.state.lastName,
+      age: this.state.age
+    };
+
+    console.log(user);
+
+    Axios
+      .post(`http://localhost:8080/api/v1/users`, { user })
+      .then(res => {
+        console.log(res);
+        if (res.status === 200)
+          this.setState({
+            name: '',
+            lastName: '',
+            age: ''
+          });
+      });
+    this.forceUpdate();
+  }
+
+  render() {
+    return (
+      <Paper elevation={3} style={this.paper}>
+        <TextField
+          label="Nombre"
+          value={this.state.name}
+          onChange={this.handleName}
+          style={this.input}
+        />
+        <TextField
+          label="Apellido"
+          value={this.state.lastName}
+          onChange={this.handleLastName}
+          style={this.input}
+        />
+        <TextField
+          label="Edad"
+          value={this.state.age}
+          onChange={this.handleAge}
+          type="number"
+          style={this.input}
+        />
+        <Button
+          variant="contained"
+          color="secondary"
+          style={{ height: 48, margin: 8 }}
+          onClick={this.handleClick}
+        >
+          Save
+        </Button>
+      </Paper>
+    );
+  }
+}
+
+InputData.propTypes = {
+  name: PropTypes.string,
+  lastName: PropTypes.string,
+  age: PropTypes.number
+}
+
+export default InputData;
---
+++ src/tableContent/TableContent.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+import { Paper, TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
+import Axios from 'axios';
+
+export class TableContent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { rows: [] }
+  }
+
+  componentDidMount() {
+    Axios
+      .get(`http://localhost:8080/api/v1/users`)
+      .then(res => {
+        this.setState({ rows: res.data })
+      });
+  }
+
+  render() {
+    return (
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Nombre</TableCell>
+              <TableCell>Apellido</TableCell>
+              <TableCell>Edad</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.rows.map(row => {
+              return (
+                <TableRow key={row.id}>
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell>{row.lastName}</TableCell>
+                  <TableCell>{row.age}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  }
+}
+
+export default TableContent;
